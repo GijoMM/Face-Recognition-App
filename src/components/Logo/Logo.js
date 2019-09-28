eeb9970652da9dd8c7a57455b91fddfeaa31f697
Giovanni <gijo.moura@gmail.com>
2019-09-28 12:59:21 -0700

    added logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import './Logo.css';
+import face from './face.png';
 
 const Logo = () => {
     return (
-        <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-            <div className="Tilt-inner"> 👽 </div>
+        <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='face logo' src={ face } /></div>
         </Tilt>
     );
 }
