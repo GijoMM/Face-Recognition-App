879798153d92ee906079d205fea05e1dbcdae485
Giovanni <gijo.moura@gmail.com>
2019-09-28 13:47:02 -0700

    Added Rank component and adjusted logo style

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Rank = () => {
     return (
-        <div></div>
+        <div>
+            <div className=' white f3'>
+                {'Andrei, your current rank is:'}
+            </div>
+            <div className=' white f1'>
+                {'#3'}
+            </div> 
+        </div>
     );
 }
 
