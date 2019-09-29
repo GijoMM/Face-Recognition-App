af8828d3f3ee6a881e32453848d5fe97fc2917ae
Giovanni <gijo.moura@gmail.com>
2019-09-28 18:05:53 -0700

    Added Clarifai package and functionality to input field and button

--- src/App.js
+++ src/App.js
@@ -19,19 +19,47 @@ const particleOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-              params={particleOptions}
-            />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm /> 
-      <FaceRecognition />
-    </div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
   );
-}
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <Particles className='particles'
+                  params={particleOptions}
+                />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit} 
+          />
+          <FaceRecognition />
+        </div>
+      );
+    }
+  }
 
 export default App;
