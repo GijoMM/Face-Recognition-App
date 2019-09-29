0623b89be81d474c6795f151f319dc359e5b80b1
Giovanni <gijo.moura@gmail.com>
2019-09-28 17:28:06 -0700

    added particles package

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
-
-
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+              params={particleOptions}
+            />
       <Navigation />
       <Logo />
       <Rank />
