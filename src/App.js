aec3d876a50a00cb28bd7ca1ead841cac028eb73
Giovanni <gijo.moura@gmail.com>
2019-10-04 18:57:11 -0700

    Implemented entries update

--- src/App.js
+++ src/App.js
@@ -84,7 +84,22 @@ class App extends React.Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))    
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+            id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entrie: count}))
+          })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))    
+      })
     .catch(err => console.log(err));
   }
 
