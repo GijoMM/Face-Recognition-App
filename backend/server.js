71db5142a730dbbc153c424100d1155bc95902bd
GijoMM <gijo.moura@gmail.com>
2019-10-29 14:27:57 -0700

    fixed variable name

--- backend/server.js
+++ backend/server.js
@@ -19,17 +19,17 @@ const db = knex({
         }
 });
 
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send(database.users)})
+app.get('/', (req, res) => {res.send(db.users)})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
