242c4eebfb5623ecaab18a75bad173f07c705c7e
GijoMM <gijo.moura@gmail.com>
2019-10-29 17:22:06 -0700

    fixing port route and adding db connection

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,8 @@ const image =  require('./controllers/image');
 const db = knex({
         client: 'pg',
         connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : '',
-        database : 'face-recognition'
+            host: process.env.DATABASE_URL,
+            ssl: true,
         }
 });
 
@@ -24,15 +22,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {res.send(db.users)})
+app.get('/', (req, res) => {res.send(`it's working!`)})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
+const PORT = process.env.PORT
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+    console.log(`app is running on port ${PORT}`);
 })
 
 /*
