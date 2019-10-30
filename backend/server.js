41ddac468e2a39847cbce100fad4812627dabdb0
GijoMM <gijo.moura@gmail.com>
2019-10-29 22:58:14 -0700

    changing port

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-const PORT = process.env.PORT
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${PORT}`);
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /*
